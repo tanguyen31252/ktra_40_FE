e2d30af6455dc4bee593931117c5f804e8fdd583
tanguyn <nguyen03012004@gmail.com>
2024-11-13 20:59:59 +0700

    dung cai nay

--- vue-project/README.md
+++ vue-project/README.md
@@ -1,4 +1,4 @@
-# lib_management
+# vue-project
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -3,11 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives
+  })
+
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
