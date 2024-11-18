f15697c55bde476aa56a6057f784dc38c1d774fc
bba2112 <hiambba2112@gmail.com>
2024-11-18 12:07:41 +0700

    BA 12h 19/11

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -3,7 +3,9 @@ import HomeView from '../views/HomeView.vue'
 import SignIn from '../components/SignIn.vue'
 import TrangChu from '../components/TrangChu.vue'
 import FTable from '../componnents/f_table.vue'
-
+import { onMounted, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+const items = reactive([]); // Khởi tạo danh sách kệ sách
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,5 +18,21 @@ const router = createRouter({
     { path: '/TrangChu', component: TrangChu },
   ],
 })
+
+
+// hàm on Mounted của Bảo Anh, phần import trên line 6,7,8 của BA
+onMounted(() => {
+  const storedShelves = localStorage.getItem('bookShelves');
+  if (storedShelves) {
+    // Nếu có dữ liệu trong LocalStorage, gán vào danh sách
+    props.items = JSON.parse(storedShelves);
+  } else {
+    // Nếu không có, khởi tạo dữ liệu mặc định
+    props.items = [
+      { id_ke: 1, so_luong_loai_sach: 0, tinh_trang: 'Trống' },
+      { id_ke: 2, so_luong_loai_sach: 0, tinh_trang: 'Trống' }
+    ];
+  }
+});
 
 export default router
